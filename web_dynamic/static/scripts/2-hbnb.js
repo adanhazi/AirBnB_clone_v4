320b7e9e7334ea028ce8578c189d7e02418e969b
Wambani01 <georgewmbn@gmail.com>
2023-05-05 01:07:28 -0700

    rendering html

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -8,4 +8,17 @@ $('document').ready(function () {
     }
     $('.amenities H4').text(Object.values(amenities).join(', '));
   });
+
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    type: 'GET'
+  }).done(function (response) {
+    if (response.status === 'OK') {
+      $('#api_status').addClass('available');
+      console.log(response.status)
+    } else {
+      $('#api_status').removeClass('available');
+      console.log(response.status)
+    }
+  });
 });
